443836bf32f29337c4408cbccb30a84aae0efb5a
floaterest <floaterest@users.noreply.github.com>
2023-04-15 20:14:16 -0400

    feat(*) astro template

---
+++ src/components/Cube.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	function cube(n: number): boolean {
+		let [c, b, i] = [0, 0, 0];
+		while (c < n) {
+			b += 6 * i;
+			c += b + 1;
+			i++;
+		}
+		return n == c;
+	}
+	let value: number = 0;
+	$: valid = Number.isInteger(value);
+</script>
+
+<div>
+	<input type="number" bind:value /> is
+	{#if !valid || !cube(value)}
+		not
+	{/if}
+	{#if valid}
+		a cube
+	{:else}
+		an integer
+	{/if}
+</div>
+
+<style lang="sass">
+	@import '../styles/html'
+	input[type=number]
+		background: unset
+		border: 1px solid $color
+		font: $mono
+		font-size: unset
+		color: $color
+		-moz-appearance: textfield
+		&::-webkit-outer-spin-button, &::-webkit-inner-spin-button 
+			-webkit-appearance: none
+
+</style>
