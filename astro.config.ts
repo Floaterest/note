69cd3f61a61e21bb8e79fa6c9f84d889dec7357b
floaterest <floaterest@users.noreply.github.com>
2023-04-16 18:20:07 -0400

    chore(astro.config) use typescript

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+const katex = {
+    trust: true,
+    strict: false,
+}
+
 // https://astro.build/config
 export default defineConfig({
+    base: 'note',
+    build:{ format: 'file' },
     integrations: [svelte(), mdx({
         remarkPlugins: { extends: [remarkMath] },
-        rehypePlugins: { extends: [rehypeKatex] }
+        rehypePlugins: { extends: [() => rehypeKatex(katex)] }
     })]
 });
