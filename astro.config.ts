fa9ec60a788f876c737292b54a08d511c42c9a65
floaterest <floaterest@users.noreply.github.com>
2023-05-03 16:52:51 -0400

    chore(astro.config) new config for astro v1

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,28 @@
-import * as path from 'path';
+import * as path from 'node:path';
 
 import { defineConfig } from 'astro/config';
+
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
-import * as shiki from 'shiki'
+// import deno from '@astrojs/deno';
 
+import * as shiki from 'shiki';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import remarkGfm from 'remark-gfm'
 
 const theme = await shiki.loadTheme(path.join(process.cwd(), 'theme.json'));
 const katex = { trust: true, fleqn: false, strict: false };
+
 // https://astro.build/config
 export default defineConfig({
-    base: 'note',
-    build: { format: 'file' },
-    markdown: { shikiConfig: { theme } },
-    integrations: [svelte(), mdx({
-        remarkPlugins: [remarkMath, remarkGfm],
+    // output: 'server',
+    // adapter: deno(),
+    trailingSlash: 'always',
+    build: { format: 'directory' },
+    markdown: {
+        shikiConfig: { theme },
+        remarkPlugins: [remarkMath],
         rehypePlugins: [() => rehypeKatex(katex)],
-    })],
+    },
+    integrations: [svelte(), mdx()],
 });
