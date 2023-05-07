30e060b44893c8ca6575c8550da6fdc9a96a7a7b
floaterest <floaterest@users.noreply.github.com>
2023-05-07 19:10:33 -0400

    doc(readme)

---
+++ README.md
@@ -0,0 +1,12 @@
+# Note
+
+## Usage
+
+1. clone repo and install packages
+2. add contents to content collections<br>
+   see [astro docs](https://docs.astro.build/en/guides/content-collections/) or [src/content/config.ts](./src/content/config.ts) for details
+3. run (p)npm dev
+
+## CD/CI
+
+In the end of [astro.yml](./.github/workflows/astro.yml), uncomment the job for deploy
--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,20 @@ const collection = defineCollection({
     }),
 });
 
-// list collection names
+/*
+e.g. for the following file structure:
+content
+├── colle-1
+│   ├── page-1.md
+│   ├── page-2.md
+│   └── page-3.md
+└── colle-2
+    ├── page-1.md
+    ├── page-2.md
+    └── page-3.md
+
+then `contents` will be ['colle-1', 'colle-2']
+*/
 const contents = (await readdir(fileURLToPath(dirname(import.meta.url)))).filter(p => !p.startsWith('config'))
 
 export const collections: { [content: string]: any } = Object.assign({}, ...contents.map(
