9cb791d9a15961fdea2330a5c830c7fec315453a
floaterest <floaterest@users.noreply.github.com>
2023-05-08 20:03:25 -0400

    doc: add example

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,4 @@
 import { z, defineCollection } from 'astro:content';
-import { readdir } from 'node:fs/promises';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 const collection = defineCollection({
     schema: z.object({
@@ -9,21 +6,7 @@ const collection = defineCollection({
     }),
 });
 
-/*
-e.g. for the following file structure:
-content
-├── colle-1
-│   ├── page-1.md
-│   ├── page-2.md
-│   └── page-3.md
-└── colle-2
-    ├── page-1.md
-    ├── page-2.md
-    └── page-3.md
-
-then `contents` will be ['colle-1', 'colle-2']
-*/
-const contents = (await readdir(fileURLToPath(dirname(import.meta.url)))).filter(p => !p.startsWith('config'))
+const contents = ['example']
 
 export const collections: { [content: string]: any } = Object.assign({}, ...contents.map(
     content => ({ [content]: collection })
