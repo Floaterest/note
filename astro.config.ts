eee17d905a78eb1e687e34cce159c972b0a0ce8c
floaterest <floaterest@users.noreply.github.com>
2023-08-14 19:48:06 -0400

    chore(config) update

--- astro.config.ts
+++ astro.config.ts
@@ -1,36 +1,39 @@
-import * as path from 'node:path';
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
-// import deno from '@astrojs/deno';
-
-import * as shiki from 'shiki';
-import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import tailwind from "@astrojs/tailwind";
-const theme = await shiki.loadTheme(path.join(process.cwd(), 'theme.json'));
+import remarkMath from 'remark-math';
+import shiki from 'shiki';
+import * as path from 'path';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import relativeLinks from 'astro-relative-links';
+import svelte from '@astrojs/svelte';
+
 const katex = {
-  trust: true,
-  fleqn: false,
-  strict: false
+    trust: true,
+    fleqn: false,
+    strict: false,
 };
+const theme = await shiki.loadTheme(path.join(process.cwd(), 'public/theme.json'));
 
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',c
-  // adapter: deno(),
-  trailingSlash: 'always',
-  build: {
-    format: 'directory'
-  },
-  // base: 'note',
-  markdown: {
-    shikiConfig: {
-      theme
+    trailingSlash: 'always',
+    compressHTML: true,
+    build: {
+        format: 'directory',
+        assets: 'assets',
     },
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [() => rehypeKatex(katex)]
-  },
-  integrations: [svelte(), mdx(), tailwind()]
+    markdown: {
+        shikiConfig: {
+            theme,
+        },
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [() => rehypeKatex(katex)],
+    },
+    integrations: [svelte(), relativeLinks(), mdx(), tailwind({
+        config: {
+            applyBaseStyles: false,
+        },
+    })],
 });
