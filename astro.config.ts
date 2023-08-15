0bcda7c5c3f19f3d4bc8477b7a101cded8dc74a7
floaterest <floaterest@users.noreply.github.com>
2023-08-15 17:56:07 -0400

    refactor(config) organise import

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
+import mdx from '@astrojs/mdx';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import relativeLinks from 'astro-relative-links';
 import { defineConfig } from 'astro/config';
+import * as path from 'path';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 import shiki from 'shiki';
-import * as path from 'path';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import relativeLinks from 'astro-relative-links';
-import svelte from '@astrojs/svelte';
 
 const katex = {
     trust: true,
